fcff606c77a505dd798c5f1f49e5a3bb182cf67f
angiemiku <ilodecena53@gmail.com>
2025-02-22 14:42:22 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+const { Client, RichPresence } = require("discord.js-selfbot-v13");
+const client = new Client();
+const express = require('express')
+const server = express();
+
+////////////////////////////////////////////////////////////////
+const Type = "PLAYING" //PLAYING / LISTENING / WATCHING / STREAMING / COMPETING
+const Name = "Genshin Impact"
+const Status = "idle" //online / idle / dnd / invisible
+const TOKEN = proccess.env.TOKEN || "DISCORD TOKEN HERE": //Your discord account TOKEN
+//////////////////////////////////////////////////////////////
+
+
+app.get('/', (req, res) => {
+  res.send('Success!!')
+});
+app.listen(port, () => {
+  console.log(`Listening to port: ${port}`);
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.username}`)
+  const pr = new RichPresence(client)
+    .setType(Type)
+    .setName(Name)
+    .setStartTimestamp("" || Date.now());
+  client.user.setPresence({ activities: [pr], status: Status });
+});
+
+client.login(TOKEN);
